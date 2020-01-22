3a591559419307216705261355618315edeacf74
izaks <iyarzaks@gmail.com>
2020-01-23 00:19:47 +0200

    fix acc small bug

--- running_scripts/best_with_labels.py
+++ running_scripts/best_with_labels.py
@@ -1,19 +1,19 @@
 import train_utils
 
 def main():
-    epochs = 3
+    epochs = 15
     conf_dic = {"WORD_EMBEDDINGS": None,
                 "WORD_EMBEDDING_DIM": 100,
-                "POS_EMBEDDING_DIM": 50,
+                "POS_EMBEDDING_DIM": 25,
                 "HID_DIM_MLP": 100,
                 "LSTM_LAYERS": 2,
                 "LOSS": "NLL",
                 "LEARNING_RATE": 0.01,
-                "ACCUMULATE_GRAD_STEPS": 20,
+                "ACCUMULATE_GRAD_STEPS": 50,
                 "LABELS_FLAG": True,
-                "WORD_DROP": False
+                "WORD_DROP": True
                 }
-    train_utils.run_test(configuration_dict=conf_dic, unique_id="best_model_labels_3_epochs", save_model=True, epochs=epochs)
+    train_utils.run_test(configuration_dict=conf_dic, unique_id="labels_15_epochs", save_model=True, epochs=epochs)
 
 if __name__ == '__main__':
     main()
